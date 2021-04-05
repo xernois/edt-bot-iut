558eb450ebc6182f8e0371f6826fddf81555e1a0
xernoi <36199633+xernoi@users.noreply.github.com>
2021-04-05 20:10:55 +0200

    ajout de nouvelles requetes sur l'api

--- api/api.py
+++ api/api.py
@@ -8,17 +8,74 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>requete invalide</h1>
-    <h3>liste des requetes faisables</h3>
+    return '''<u><h1>API EDT IUT</h1></u>
     <ul>
-    <li>/api/edt/a1       <i>(retourne le json de l'edt des a1)</i></li>
-    </ul>
+        <li>
+            <h3>liste des requetes faisables</h3>
+            <ul>
+            <li>/api/edt/a1       <i>(retourne le json de l'edt des a1)</i></li>
+            <li>/api/edt/a2       <i>(retourne le json de l'edt des a1)</i></li>
+            <li>/api/edt/a3       <i>(retourne le json de l'edt des a1)</i></li>
+            </ul>
+        </li>
+        <li>
+            <h3>liste des options</h3>
+            <ul>
+            <li>?s=X       <i>(retourne l'edt de la semaine X)</i></li>
+            </ul>
+        </li>
+    <ul>
     '''
 
+    
 @app.route('/api/edt/a1', methods=['GET'])
 def edt_a1():
-    with open('a1.json') as f:
-        content = json.load(f)
+    if request.args.get('s') == None:
+        semaine = 1
+    else:
+        semaine = request.args.get('s')
+    try:
+        with open('edt/A1_S'+str(semaine)+'.json') as f:
+            content = json.load(f)
+    except IOError:
+        return 'L\'edt de la semaine '+str(semaine)+' n\'est pas disponible'
     return content
+
+@app.route('/api/edt/a2', methods=['GET'])
+def edt_a2():
+    if request.args.get('s') == None:
+        semaine = 1
+    else:
+        semaine = request.args.get('s')
+    try:
+        with open('edt/A2_S'+str(semaine)+'.json') as f:
+            content = json.load(f)
+    except IOError:
+        return 'L\'edt de la semaine '+str(semaine)+' n\'est pas disponible'
+    return content
+
+@app.route('/api/edt/a3', methods=['GET'])
+def edt_a3():
+    if request.args.get('s') == None:
+        semaine = 1
+    else:
+        semaine = request.args.get('s')
+    try:
+        with open('edt/A3_S'+str(semaine)+'.json') as f:
+            content = json.load(f)
+    except IOError:
+        return 'L\'edt de la semaine '+str(semaine)+' n\'est pas disponible'
+    return content
+
+
+
+
+
+
+
+
+
+
+
 
 app.run()
