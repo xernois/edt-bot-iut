4d8f7e6c516ed7c38e05fa991d9070a755211746
xernoi <36199633+xernoi@users.noreply.github.com>
2021-04-05 15:03:54 +0200

    recuperation des pdf

--- api/api.py
+++ api/api.py
@@ -1,76 +1,24 @@
-import flask
+import flask, json
 from flask import jsonify, request
 from selenium import webdriver
 from selenium.webdriver.common import keys
 
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-books = [
-    {'id': 0,
-        'title': 'A Fire Upon the Deep',
-        'author': 'Vernor Vinge',
-        'first_sentence': 'The coldsleep itself was dreamless.',
-        'year_published': '1992'},
-    {'id': 1,
-        'title': 'The Ones Who Walk Away From Omelas',
-        'author': 'Ursula K. Le Guin',
-        'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-        'published': '1973'},
-    {'id': 2,
-        'title': 'Dhalgren',
-        'author': 'Samuel R. Delany',
-        'first_sentence': 'to wound the autumnal city.',
-        'published': '1975'}
-]
-
-
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+    return '''<h1>requete invalide</h1>
+    <h3>liste des requetes faisables</h3>
+    <ul>
+    <li>/api/edt/a1       <i>(retourne le json de l'edt des a1)</i></li>
+    </ul>
+    '''
 
-
-@app.route('/api/a1', methods=['GET'])
+@app.route('/api/edt/a1', methods=['GET'])
 def edt_a1():
-    results = ""
-    driver = webdriver.Chrome(
-        executable_path="C:\Program Files\chromedriver\chromedriver.exe")
-    driver.get("http://edt-iut-info.unilim.fr/edt/A1/")
-    l = driver.find_elements_by_xpath("/html/body/table/tbody/tr")
-    for i in l:
-        results += "\n" + i.text
-    driver.quit()
-    return results
-
-
-@app.route('/api/a2', methods=['GET'])
-def edt_a2():
-    results = ""
-    driver = webdriver.Chrome(
-        executable_path="C:\Program Files\chromedriver\chromedriver.exe")
-    driver.get("http://edt-iut-info.unilim.fr/edt/A2/")
-    l = driver.find_elements_by_xpath("/html/body/table/tbody/tr")
-    for i in l:
-        results += "\n" + i.text
-    driver.quit()
-    return results
-
-
-@app.route('/api/books', methods=['GET'])
-def api_id():
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
-
-    results = []
-
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
-    return jsonify(results)
-
+    with open('a1.json') as f:
+        content = json.load(f)
+    return content
 
 app.run()
--- api/getEdt.py
+++ api/getEdt.py
@@ -1,7 +1,15 @@
-import datetime, json                                                                                   # import de json pour manipuler des json et de datetime pour manipuler des dates
+import datetime, json, requests
+from pdf2image.exceptions import (
+    PDFInfoNotInstalledError,
+    PDFPageCountError,
+    PDFSyntaxError
+)
 from selenium import webdriver                                                                          # import des webdrivers depuis la librairy selenium
 from selenium.webdriver.common import keys                                                              # 
+from pdf2image import convert_from_path
 
+
+import tempfile
 
 
 class Edt():                                                                                            # Definition de l'objet Edt qui a pour attribut un nom une date et une taille
@@ -14,13 +22,20 @@ class Edt():                                                                    
 
 
 
-def download_edt():
-    print('existe po')
+def download_edt(file_name):
+    print('je doit dl ' + file_name);
+    response = requests.get('http://edt-iut-info.unilim.fr/edt/' + file_name)
+    print(file_name.split('/')[1])
+    with open('edt/'+file_name.split('/')[1], 'wb') as f:
+        f.write(response.content)
+    pages = convert_from_path('edt/' + file_name.split('/')[1], 500)
+    for page in pages:
+        page.save("test.jpg",'JPEG')
 
 
 
 def fetch_edt():                                                                                        # fonction pour recuperer tout les edts 
-    driver = webdriver.Firefox(executable_path="C:\\Program Files\\browser_Driver\\geckodriver.exe")    # creation d'un nouveau driver pour naviguer sur internet
+    driver = webdriver.Firefox(executable_path="C:\\browser_driver\\geckodriver.exe")                   # creation d'un nouveau driver pour naviguer sur internet
     driver.minimize_window()                                                                            # reduction du navigateuur pour pas le voir sur l'ecrans
     driver.get("http://edt-iut-info.unilim.fr/edt/")                                                    # recupere le contenu de la page internet
     folders = driver.find_elements_by_xpath("/html/body/table/tbody/tr")                                # recuperer la liste des dossier contenant des edts sur le site
@@ -30,22 +45,18 @@ def fetch_edt():                                                                
             driver.get("http://edt-iut-info.unilim.fr/edt/"+info[0])                                    # le driver charge un des dossier contenant les edts 
             edts = driver.find_elements_by_xpath("/html/body/table/tbody/tr")                           # le driver recupere la liste des edts
             list_edt = []                                                                               #
-            for j in range(3, len(edts)-1):                                                             # boucle sur la liste des edts dans le dossier courrant
+            for j in range(3, len(edts) - 1):
                 info_edt = edts[j].text.split(" ")                                                      # le texte est couper pour en recuperer des données
                 date_edt = [int(data) for data in info_edt[1].split("-")]                               # recuperation des données correspondant a la date de publication
                 heure_edt = [int(data) for data in info_edt[2].split(":")]                              # recuperation des données correspondant a l'heure de publication
                 date = datetime.datetime(date_edt[0],date_edt[1],date_edt[2],heure_edt[0],heure_edt[1]) # creation d'une date pour l'obj Edt
-                list_edt.append(Edt(info_edt[0],date,info_edt[3]))                                      # creation d'un obj Edt pour chaques edts, cet obj est stocker dans la liste list_edt 
-            driver.back()                                                                               # le driver fais un retour en arriere pour revenir a la page avec la liste des dossier contenant les edts
-            print(info[0]+":")                                                                          # affichage du nom du repertoire contenant les edts
-            for edt in list_edt:                                                                        # boucle sur le nombre d'obj Edt
+                list_edt.append(Edt(info_edt[0], date, info_edt[3]))
                 try:
-                    with open(edt.nom.split('.')[0]+'.json') as f:
-                        print(f.readlines())
+                    with open('edt/' + info_edt[0].split('.')[0] + '.pdf') as f:
+                        print("je suis la")
                 except IOError:
-                    download_edt()
-                print(edt.info_json())                                                                  # print le text retourner par la methode info_json() de l'obj Edt
-            print("\n\n")                                                                               #
+                    download_edt(info[0]+info_edt[0])
+            driver.back()                                                                               # le driver fais un retour en arriere pour revenir a la page avec la liste des dossier contenant les edts                                                                       # affichage du nom du repertoire contenant les edts                                                                             #
     driver.quit()                                                                                       # fermeture du driver
 
 
