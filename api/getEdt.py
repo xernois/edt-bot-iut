6e66f7f87d11cecb275acc64fdd227db96fd5263
xernoi <36199633+xernoi@users.noreply.github.com>
2021-04-05 16:32:11 +0200

    suppression des images apres lecture

--- api/getEdt.py
+++ api/getEdt.py
@@ -1,16 +1,6 @@
-import datetime, json, requests, pdf2image
+import datetime, json, requests, pdf2image, os
 from selenium import webdriver                                                                          # import des webdrivers depuis la librairy selenium
-from selenium.webdriver.common import keys                                                              # 
-
-
-class Edt():                                                                                            # Definition de l'objet Edt qui a pour attribut un nom une date et une taille
-    def __init__(self, nom, date, taille):                                                              # constructeur 3 parametre
-        self.nom = nom                                                                                  #
-        self.date = date                                                                                #
-        self.taille = taille                                                                            #
-    def info_json(self):                                                                                # Methode pour retourner les infos de l'Edt sous forme de JSON
-        return {'nom': self.nom,'date':self.date.strftime("%Y-%m-%d %H:%M"),'taille':self.taille}       # info sous forme de js avec la date au format (Annee-Mois-Jour Heure:Minutes)
-
+from selenium.webdriver.common import keys                                                              
 
 
 def download_edt(file_name):
@@ -21,6 +11,8 @@ def download_edt(file_name):
     for page in pages:
         page.save('edt/'+file_name.split('/')[1].split('.')[0]+".jpg", 'JPEG')
         # main.main('edt/'+file_name.split('/')[1].split('.')[0]+".jpg")
+        if os.path.exists('edt/'+file_name.split('/')[1].split('.')[0]+".jpg"):
+            os.remove('edt/'+file_name.split('/')[1].split('.')[0]+".jpg")
 
 
 def fetch_edt():                                                                                        # fonction pour recuperer tout les edts 
@@ -32,17 +24,15 @@ def fetch_edt():                                                                
         info = folders[i].text.split(" ")                                                               # recuperation du nom du dossier pour rentrer dedans 
         if(info[0].startswith('A')):                                                                    # 
             driver.get("http://edt-iut-info.unilim.fr/edt/"+info[0])                                    # le driver charge un des dossier contenant les edts 
-            edts = driver.find_elements_by_xpath("/html/body/table/tbody/tr")                           # le driver recupere la liste des edts
-            list_edt = []                                                                               #
+            edts = driver.find_elements_by_xpath("/html/body/table/tbody/tr")                           # le driver recupere la liste des edts                                                                            #
             for j in range(3, len(edts) - 1):
                 info_edt = edts[j].text.split(" ")                                                      # le texte est couper pour en recuperer des données
                 date_edt = [int(data) for data in info_edt[1].split("-")]                               # recuperation des données correspondant a la date de publication
                 heure_edt = [int(data) for data in info_edt[2].split(":")]                              # recuperation des données correspondant a l'heure de publication
                 date = datetime.datetime(date_edt[0],date_edt[1],date_edt[2],heure_edt[0],heure_edt[1]) # creation d'une date pour l'obj Edt
-                list_edt.append(Edt(info_edt[0], date, info_edt[3]))
                 try:
-                    with open('edt/' + info_edt[0].split('.')[0] + '.jpg') as f:
-                        print("je suis la")
+                    with open('edt/' + info_edt[0].split('.')[0] + '.json') as f:
+                        pass
                 except IOError:
                     download_edt(info[0]+info_edt[0])
             driver.back()                                                                               # le driver fais un retour en arriere pour revenir a la page avec la liste des dossier contenant les edts                                                                       # affichage du nom du repertoire contenant les edts                                                                             #
