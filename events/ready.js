0a788fd167913419e1d5b2a2eb92e771acd91e45
xernoi <36199633+xernoi@users.noreply.github.com>
2021-06-09 18:25:46 +0200

    nettoyage du repo

--- events/ready.js
+++
@@ -1,6 +0,0 @@
-const vers = require("../utilities/mainConf.json");
-
-module.exports = (_client) => {
-  console.log(`EDT-IUT v${vers.version} bot has been launched successfully`);
-  console.log(`===========================================================`);
-}
--- utilities/edtFetch.js
+++
@@ -1,5 +0,0 @@
-const fetch = require('node-fetch');
-
-module.exports = function edtFetch (annee, groupe, semaine){
-    return fetch(`http://109.220.5.61/api/edt/a${annee}?s=${semaine}&g=${groupe}`)
-}
